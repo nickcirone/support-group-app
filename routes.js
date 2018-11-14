e246d75b53313272599f023e40af555e0a19b158
Yasaleh <44013644+Yasaleh@users.noreply.github.com>
2018-11-14 12:05:34 -0500

    Add files via upload

--- routes.js
+++ routes.js
@@ -2,38 +2,114 @@ var passport = require('passport');
 var User = require('./models/users');
 var Profile = require('./models/profile');
 
-async function asyncFindProfileById(id) {
-    var currProfile;
-    Profile.findById(id, function (err, currentProfile) {
-        if (err) {
-            console.log('error finding profile');
-        } else {
-            currProfile = currentProfile;
-            return currProfile;
-        }
-    });
+function checkServices(body) {
+    var newServices = [];
+    if (body.Surgery) {
+        newServices.push(body.Surgery);
+    }
+    if (body.Neurology) {
+        newServices.push(body.Neurology);
+    }
+    if (body.ENTSurgery) {
+        newServices.push(body.ENTSurgery);
+    }
+    if (body.OrthopedicSurgery) {
+        newServices.push(body.OrthopedicSurgery);
+    }
+    if (body.CardiothoracicSurgery) {
+        newServices.push(body.CardiothoracicSurgery);
+    }
+    if (body.UrologySurgery) {
+        newServices.push(body.UrologySurgery);
+    }
+    if (body.Burns) {
+        newServices.push(body.Burns);
+    }
+    if (body.GeneralMedicine) {
+        newServices.push(body.GeneralMedicine);
+    }
+    if (body.Cardiology) {
+        newServices.push(body.Cardiology);
+    }
+    if (body.Endocrinology) {
+        newServices.push(body.Endocrinology);
+    }
+    if (body.GI) {
+        newServices.push(body.GI);
+    }
+    if (body.HematologyOncology) {
+        newServices.push(body.HematologyOncology);
+    }
+    if (body.Nephrology) {
+        newServices.push(body.Nephrology);
+    }
+    if (body.Pulmonology) {
+        newServices.push(body.Pulmonology);
+    }
+    if (body.BoneMarrowTransplant) {
+        newServices.push(body.BoneMarrowTransplant);
+    }
+    return newServices;
 }
 
-async function asyncGetFriend(id) {
-    
-}
-
-async function asyncGetFriends(iDs) {
-    var currFriends = [];
-    for (var i = 0; i < iDs.length; i++) {
-        User.findById(fIds[i], function(err, currentUser) {
-            currFriends[i] = currentUser;
-        });
-    }   
-    return currFriends;
+function checkInterests(body) {
+    var newInterests = [];
+    if (body.videoGames) {
+        newInterests.push(body.videoGames);
+    }
+    if (body.cooking) {
+        newInterests.push(body.cooking);
+    }
+    if (body.artsCrafts) {
+        newInterests.push(body.artsCrafts);
+    }
+    if (body.legos) {
+        newInterests.push(body.legos);
+    }
+    if (body.building) {
+        newInterests.push(body.building);
+    }
+    if (body.writing) {
+        newInterests.push(body.writing);
+    }
+    if (body.reading) {
+        newInterests.push(body.reading);
+    }
+    if (body.tv) {
+        newInterests.push(body.tv);
+    }
+    if (body.movies) {
+        newInterests.push(body.movies);
+    }
+    if (body.boardCardGames) {
+        newInterests.push(body.boardCardGames);
+    }
+    if (body.sports) {
+        newInterests.push(body.sports);
+    }
+    if (body.swimming) {
+        newInterests.push(body.swimming);
+    }
+    if (body.boating) {
+        newInterests.push(body.boating);
+    }
+    if (body.fishing) {
+        newInterests.push(body.fishing);
+    }
+    return newInterests;
 }
 
 module.exports = function(app) {
+
+    // homepage route (after logging in)
+
     app.get('/',
         require('connect-ensure-login').ensureLoggedIn(),
         function(req, res) {
             res.render('home', { user: req.user });
     });
+
+    // admin routes
 
     app.get('/admin',
         require('connect-ensure-login').ensureLoggedIn(),
@@ -41,19 +117,21 @@ module.exports = function(app) {
             res.render('admin', {user: req.user});
     });
 
-    /*
-    app.post('/register', function (req, res) {
-        User.register
-    });
-    */
+    // login routes
 
     app.get('/login',
         function(req, res) {
             res.render('login');
     });
+
+    app.get('/loginFail',
+        function(req, res) {
+            res.render('loginFail');
+        }
+    )
   
     app.post('/login', 
-        passport.authenticate('local', { failureRedirect: '/login' }),
+        passport.authenticate('local', { failureRedirect: '/loginFail' }),
         function(req, res) {
             if (req.user.role === 'admin') {
                 res.redirect('/admin');
@@ -61,12 +139,16 @@ module.exports = function(app) {
                 res.redirect('/');
             }
     });
+
+    // logout route
   
     app.get('/logout',
         function(req, res){
             req.logout();
             res.redirect('/');
     });
+
+    // profile route
 
     app.get('/profile',
         require('connect-ensure-login').ensureLoggedIn(),
@@ -84,7 +166,14 @@ module.exports = function(app) {
                                 '_id': { $in: profile.friendIds }
                             }, function(err, users) {
                                 friends = users;
-                                res.render('profile', { user: req.user, profile: profile, friends: friends });
+                                var FIDs = friends.map((account)=>{return account.profileId});
+                                var friendProfiles;
+                                Profile.find({
+                                    '_id': { $in: FIDs }
+                                }, function(err, friend_profiles) {
+                                    friendProfiles = friend_profiles
+                                res.render('profile', { user: req.user, profile: profile, friends: friends, friendProfiles: friendProfiles });
+                                });
                             });
                         }
                     }); 
@@ -98,11 +187,80 @@ module.exports = function(app) {
             }
     });
 
+    // profile edit routes
+
+    app.get('/profileEdit',
+        require('connect-ensure-login').ensureLoggedIn(),
+        function(req, res) {
+            if (req.user.role === 'patient') {
+                if (req.user.profileId !== null) {
+                    var profile;
+                    Profile.findById(req.user.profileId, function (err, currentProfile) {
+                        if (err) {
+                            console.log('error finding profile');
+                        } else {
+                            var friends = [];
+                            profile = currentProfile;
+                            User.find({
+                                '_id': { $in: profile.friendIds }
+                            }, function(err, users) {
+                                friends = users;
+                                res.render('profileEdit', { user: req.user, profile: profile, friends: friends });
+                            });
+                        }
+                    }); 
+                } else {
+                    console.log('Profile not found.');
+                    res.redirect('/');
+                }
+            } else {
+                console.log("Not a patient");
+                res.redirect('/');
+            }
+        }
+    );
+
+    app.post('/profileEdit',
+        require('connect-ensure-login').ensureLoggedIn(),
+        function(req, res) {
+            Profile.findById(req.user.profileId, function (err, profile) {
+                if (err) {
+                    console.log('Error finding profile to update.');
+                    res.redirect('/profile');
+                } else {
+                    var servicesArr = checkServices(req.body);
+                    var interestsArr = checkInterests(req.body);
+                    profile.set({ avatar: req.body.avatar });
+                    profile.set({ birthdate: req.body.birthdate });
+                    profile.set({ age: req.body.age });
+                    profile.set({ devAge: req.body.devAge });
+                    profile.set({ genderId: req.body.genderId });
+                    profile.set({ bio: req.body.bio });
+                    profile.set({ services: servicesArr });
+                    profile.set({ interests: interestsArr});
+                    profile.save(function (err) {
+                      if (err) {
+                          console.log('Error updating profile.')
+                          res.redirect('/profile');
+                      } else {
+                          res.redirect('/profile');
+                      }
+                    });
+                }
+                
+            });
+        }
+    )
+
+    // messaging routes
+
     app.get('/messages',
         require('connect-ensure-login').ensureLoggedIn(),
         function(req, res) {
             res.render('messages');
     });
+
+    // matches routes
 
     app.get('/matches',
         require('connect-ensure-login').ensureLoggedIn(),
@@ -115,22 +273,42 @@ module.exports = function(app) {
                             console.log('error finding profile');
                         } else {
                             var matches = [];
-                            var sent = [];
+                            var matchProfileIds=[];
+                            var matchProfiles = [];
                             var received = [];
+                            var receivedPIds =[];
+                            var receivedProfiles=[];
+                            var sent = [];
+                            var sentPIds = [];
+                            var sentProfiles=[];
                             profile = currentProfile;
                             User.find({
                                 '_id': { $in: profile.matchIds }
                             }, function(err, users) {
                                 matches = users;
+                                matchProfileIds = matches.map((account)=>{return account.profileId});
                                 User.find({
                                     '_id': { $in: profile.sentPendingFriendIds}
                                 }, function(err, sentPending) {
                                     sent = sentPending;
+                                    sentPIds = sent.map((account)=>{return account.profileId})
                                     User.find({
                                         '_id': { $in: profile.recvPendingFriendIds}
                                     }, function(err, recvPending) {
                                         received = recvPending;
-                                        res.render('matches', { user: req.user, profile: profile, matches: matches, sent: sent, received: received });
+                                        receivedPIds = received.map((account)=>{return account.profileId});
+                                        Profile.find({'_id':{$in: matchProfileIds}}, function(err,match_Profiles){
+                                            matchProfiles = match_Profiles;
+                                            Profile.find({'_id':{$in: receivedPIds}},function(err,recProfiles){
+                                                receivedProfiles = recProfiles;
+                                                Profile.find({'_id':{$in: sentPIds}},function(err,sent_Profiles){
+                                                    sentProfiles = sent_Profiles;
+                                                    res.render('matches', { user: req.user, profile: profile, matches: matches,
+                                                        sent: sent, received: received, matchProfiles: matchProfiles, 
+                                                        receivedProfiles: receivedProfiles, sentProfiles: sentProfiles });
+                                                });
+                                            });
+                                        });
                                     });
                                 });
                             });
@@ -145,4 +323,100 @@ module.exports = function(app) {
                 res.redirect('/');
             }
     });
+
+    app.post('/matches',
+        require('connect-ensure-login').ensureLoggedIn(),
+        function(req, response) {
+        var selectedUserId = req.body.userId;
+        var selectedUserProfile = req.body.userProfileId;
+
+            if(req.body.postType=="match"){
+                //matched user is added to logged in users sentPendingFriendsIds
+               
+                var removedMatch = [];
+                var addsentFriendIds =[];
+               
+                var selectedUserRemovedMatch = [];
+                var selectedUserRecvPending =[];
+                //find user profile
+                Profile.findById(req.user.profileId, function (err, currentProfile) {
+                    //remove selected user from matches array
+                    removedMatch = currentProfile.matchIds.filter((id)=>{return id != selectedUserId});
+
+                    //add selected user to sentPendginFriendsIds array
+                    addsentFriendIds = currentProfile.sentPendingFriendIds;
+                    addsentFriendIds.push(selectedUserId)
+
+                        //find match profile
+                        Profile.findById(selectedUserProfile, function (err, matchProfile) {
+                            selectedUserRemovedMatch = matchProfile.matchIds.filter((id)=>{return id != req.user._id});
+                            
+                            selectedUserRecvPending = matchProfile.recvPendingFriendIds;
+                            selectedUserRecvPending.push(req.user._id)
+
+                                //update match db
+                                Profile.findOneAndUpdate({_id: selectedUserProfile},{"$set":{matchIds: selectedUserRemovedMatch, recvPendingFriendIds: selectedUserRecvPending}},
+                                function(err,res){ 
+                                    if(err){throw err;
+                                    }else{ console.log("Recieved match!")};
+                                    
+                                    //update user db
+                                    Profile.findOneAndUpdate({_id: req.user.profileId},{"$set":{matchIds: removedMatch, sentPendingFriendIds: addsentFriendIds}},
+                                    function(err,res){ 
+                                        if(err){throw err;
+                                        }else{ console.log("Sent match request");
+                                        
+                                        var myUser;
+                                        User.findById(selectedUserId,function(err, user){
+                                            myUser = user;
+                                            response.send({matchProfile:matchProfile, myUser:myUser})
+                                        })
+                                        };
+                                    });
+
+                                });
+                        
+        
+                        });
+
+
+                });
+                
+            }else if(req.body.postType=="accepted"){
+                
+                var removedPendingFriend = [];
+                var newfriendIds =[];
+                var removedAcceptedSentIds = [];
+                var acceptedSentFriends = [];
+                Profile.findById(req.user.profileId, function (err, currentProfile) {
+                    removedPendingFriend = currentProfile.recvPendingFriendIds.filter((id)=>{return id != selectedUserId});
+                    newfriendIds = currentProfile.friendIds;
+                    newfriendIds.push(selectedUserId);
+
+                    //find friend Profile
+                    Profile.findById(selectedUserProfile, function (err, friendProfile) {
+                        removedAcceptedSentIds = friendProfile.sentPendingFriendIds;
+                        removedAcceptedSentIds.splice(removedAcceptedSentIds.indexOf(req.user._id),1)
+
+                        acceptedSentFriends = friendProfile.friendIds;
+                        acceptedSentFriends.push(req.user._id);
+
+                        //update db
+                        Profile.findOneAndUpdate({_id: selectedUserProfile},{"$set":{sentPendingFriendIds: removedAcceptedSentIds, friendIds: acceptedSentFriends}},
+                        function(err,res){ if(err){throw err;
+                        }else{ console.log("Friend Accepted!")};
+                        
+                            //update db
+                            Profile.findOneAndUpdate({_id: req.user.profileId},{"$set":{recvPendingFriendIds: removedPendingFriend, friendIds: newfriendIds}},
+                            function(err,res){ if(err){throw err;
+                            }else{ 
+                                console.log("Added new Friend!")};
+                                response.send({selectedUserProfile:selectedUserProfile})
+                            });
+                        });
+                    });
+                });
+            }
+
+        });
 };
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-var path = require('path');
 var express = require('express');
 var mongoose = require('mongoose');
 var passport = require('passport');
@@ -9,8 +8,11 @@ var app = express();
 app.set('port', process.env.PORT || 3001);
 
 // Configure view engine to render EJS templates.
-app.set('views', __dirname + '/views');
+//app.engine('html',cons.swig)
+//app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
+//app.set('view engine', 'html');
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
@@ -31,26 +33,43 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// Connect to mongo !!!! USES MY mLAB info, so replace URI with your own development db. 
-mongoose.connect("mongodb://ncirone:nRsoQloNthstY1@ds227853.mlab.com:27853/support_group_dev", { useNewUrlParser: true });
+// Connect to mongo !!!! USES MY (Nick's) mLAB info, so replace URI with your own development db. 
+//mongoose.connect("mongodb://ncirone:nRsoQloNthstY1@ds227853.mlab.com:27853/support_group_dev", { useNewUrlParser: true });
+mongoose.connect("mongodb://admin:admin1@ds125673.mlab.com:25673/support-app-test", { useNewUrlParser: true });
 
+var makeName = require('./helpers/nameGen.js');
+
+console.log(makeName());
+console.log(makeName());
+
+// Randomly generate ObjectIds for Profiles
 var profileOneId = mongoose.Types.ObjectId();
 var profileTwoId = mongoose.Types.ObjectId();
 var profileThreeId = mongoose.Types.ObjectId();
 var profileFourId = mongoose.Types.ObjectId();
 var profileFiveId = mongoose.Types.ObjectId();
+var profileSixId = mongoose.Types.ObjectId();
+var profileSevenId = mongoose.Types.ObjectId();
+var profileEightId = mongoose.Types.ObjectId();
+var profileNineId = mongoose.Types.ObjectId();
+var profileTenId = mongoose.Types.ObjectId();
 
+// Randomly generate ObjectIds for Users
 var userOneId = mongoose.Types.ObjectId();
 var userTwoId = mongoose.Types.ObjectId();
 var userThreeId = mongoose.Types.ObjectId();
 var userFourId = mongoose.Types.ObjectId();
 var userFiveId = mongoose.Types.ObjectId();
-
+var userSixId = mongoose.Types.ObjectId();
+var userSevenId = mongoose.Types.ObjectId();
+var userEightId = mongoose.Types.ObjectId();
+var userNineId = mongoose.Types.ObjectId();
+var userTenId = mongoose.Types.ObjectId();
 // Helper function for registering Users 
 function registerUser(user, pass) {
   User.register(user, pass, function(err) {
     if (err) {
-      console.log('error while user ' + user.username + ' register!', err);
+      console.log('error while registering user: ' + user.username, err);
     } else {
       console.log('user ' + user.username + ' registered!');
     }
@@ -72,61 +91,151 @@ function registerProfile(profile) {
 var profileOne = new Profile(
   {
     _id: profileOneId,
-    age: 13,
+    avatar: 'apple_one.jpg',
+    age: 9,
+    genderId: 'male',
     bio: 'placeholder bio! thanks 4 reading',
-    interests: 'sports',
-    services: 'Burns',
-    friendIds: [userTwoId, userFourId],
-    matchIds: [userThreeId],
-    sentPendingFriendIds: [userFiveId],
+    interests: ['sports', 'cooking', 'video games'],
+    services: ['Burns'],
+    friendIds: [],
+    matchIds: [userThreeId, userFourId,userSixId, userSevenId],
+    sentPendingFriendIds: [userFiveId, userEightId, userTwoId],
+    recvPendingFriendIds: [userNineId, userTenId],
   }
 );
 
 var profileTwo = new Profile(
   {
     _id: profileTwoId,
-    age: 12,
+    avatar: 'banana_two.jpg',
+    age: 10,
+    genderId: 'female',
     bio: 'placeholder bio! thanks 4 reading',
-    interests: 'sports',
-    services: 'Surgery',
-    friendIds: [userOneId, userThreeId, userFourId],
-    matchIds: [userTwoId],
+    interests: ['sports'],
+    services: ['Surgery'],
+    friendIds: [userFiveId],
+    matchIds: [userOneId],
+    sentPendingFriendIds: [userThreeId, userFourId, userEightId, userNineId, userSevenId, userTenId, userSixId],
+    
   }
 );
 
 var profileThree = new Profile(
   {
     _id: profileThreeId,
-    age: 14,
+    avatar: 'cake.png',
+    age: 11,
+    genderId: 'female',
     bio: 'placeholder bio! thanks 4 reading',
-    interests: 'sports',
-    services: 'GI',
+    interests: ['sports'],
+    services: ['GI'],
     friendIds: [userTwoId, userFourId],
-    matchIds: [userThreeId],
+    matchIds: [userEightId],
     sentPendingFriendIds: [userFiveId],
-    recvPendingFriendIds: [userOneId],
+    recvPendingFriendIds: [userSixId],
   }
 );
 
 var profileFour = new Profile(
   {
     _id: profileFourId,
-    age: 13,
+    avatar: 'circus.png',
+    age: 12,
+    genderId: 'male',
     bio: 'placeholder bio! thanks 4 reading',
-    interests: 'fishing',
-    services: 'Cardiology',
-    friendIds: [userOneId, userTwoId, userThreeId, userFourId],
+    interests: ['fishing'],
+    services: ['Cardiology'],
+    friendIds: [userTwoId, userThreeId],
+    matchIds: [userSevenId],
+    sentPendingFriendIds: [userFiveId],
+    recvPendingFriendIds: [userSixId],
   }
 );
 
 var profileFive = new Profile(
   {
     _id: profileFiveId,
-    age: 11,
+    avatar: 'game.png',
+    age: 13,
+    genderId: 'female',
     bio: 'placeholder bio! thanks 4 reading',
-    interests: 'fishing',
-    services: 'Neurosurgery',
+    interests: ['fishing'],
+    services: ['Neurosurgery'],
     friendIds: [userFourId],
+  }
+);
+var profileSix = new Profile(
+  {
+    _id: profileSixId,
+    avatar: 'safe.png',
+    age: 14,
+    genderId: 'female',
+    bio: 'placeholder bio! thanks 4 reading',
+    interests: ['fishing'],
+    services: ['Neurosurgery'],
+    friendIds: [userTwoId],
+    matchIds: [userThreeId, userFourId, userSevenId],
+    sentPendingFriendIds: [userFiveId],
+  }
+);
+
+var profileSeven = new Profile(
+  {
+    _id: profileSevenId,
+    avatar: 'submarine.png',
+    age: 15,
+    genderId: 'female',
+    bio: 'placeholder bio! thanks 4 reading',
+    interests: ['sports'],
+    services: ['Neurosurgery'],
+    friendIds: [userTwoId],
+    matchIds: [userThreeId, userSixId],
+    sentPendingFriendIds: [userFiveId],
+  }
+);
+
+var profileEight = new Profile(
+  {
+    _id: profileEightId,
+    avatar: 'pear.png',
+    age: 16,
+    genderId: 'female',
+    bio: 'placeholder bio! thanks 4 reading',
+    interests: ['sports'],
+    services: ['Neurosurgery'],
+    friendIds: [userTwoId],
+    matchIds: [userThreeId, userSixId],
+    sentPendingFriendIds: [userFiveId],
+  }
+);
+
+var profileNine = new Profile(
+  {
+    _id: profileNineId,
+    avatar: 'tomato.png',
+    age: 17,
+    genderId: 'female',
+    bio: 'placeholder bio! thanks 4 reading',
+    interests: ['sports'],
+    services: ['Neurosurgery'],
+    friendIds: [userTwoId],
+    matchIds: [userThreeId, userSixId],
+    sentPendingFriendIds: [userFiveId],
+  }
+);
+
+var profileTen = new Profile(
+  {
+    _id: profileTenId,
+    avatar: 'peach.png',
+    age: 17,
+    genderId: 'female',
+    bio: 'placeholder bio! thanks 4 reading',
+    interests: ['sports'],
+    services: ['Neurosurgery'],
+    friendIds: [userSevenId],
+    matchIds: [userEightId, userNineId],
+    sentPendingFriendIds: [userFiveId],
   }
 );
 
@@ -192,6 +301,57 @@ var endive_five = new User(
   }
 );
 
+var mango_six = new User(
+  {
+    _id: userSixId,
+    username: 'mango six',
+    email: 'mango@email.com',
+    role: 'patient',
+    profileId: profileSixId,
+  }
+);
+
+var banana_seven = new User(
+  {
+    _id: userSevenId,
+    username: 'banana seven',
+    email: 'banana@email.com',
+    role: 'patient',
+    profileId: profileSevenId,
+  }
+);
+
+var pear_eight = new User(
+  {
+    _id: userEightId,
+    username: 'pear eight',
+    email: 'pear@email.com',
+    role: 'patient',
+    profileId: profileEightId,
+  }
+);
+
+var tomato_nine = new User(
+  {
+    _id: userNineId,
+    username: 'tomato nine',
+    email: 'tomato@email.com',
+    role: 'patient',
+    profileId: profileNineId,
+  }
+);
+
+var peach_ten = new User(
+  {
+    _id: userTenId,
+    username: 'peach ten',
+    email: 'peach@email.com',
+    role: 'patient',
+    profileId: profileTenId,
+  }
+);
+
+
 /*
 // Save dummy profiles to Mongo
 registerProfile(profileOne);
@@ -199,13 +359,22 @@ registerProfile(profileTwo);
 registerProfile(profileThree);
 registerProfile(profileFour);
 registerProfile(profileFive);
-
+registerProfile(profileSix);
+registerProfile(profileSeven);
+registerProfile(profileEight);
+registerProfile(profileNine);
+registerProfile(profileTen);
 // Register users in Mongo
 registerUser(apple_one, 'apple_one');
 registerUser(banana_two, 'banana_two');
 registerUser(coconut_three, 'coconut_three');
 registerUser(durian_four, 'durian_four');
 registerUser(endive_five, 'endive_five');
+registerUser(mango_six, 'mango_six');
+registerUser(banana_seven, 'banana_seven');
+registerUser(pear_eight, 'pear_eight');
+registerUser(tomato_nine, 'tomato_nine');
+registerUser(peach_ten, 'peach_ten');
 */
 
 require('./routes')(app);
